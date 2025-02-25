2cce2480a7d2e9dafb31cfdd991d4c9a6c7d9ce7
nate merkl <nattyesquire@gmail.com>
2025-02-25 08:11:15 -0900

    Initial commit

---
+++ Larkon/resources/js/components/form-wizard.js
@@ -0,0 +1,14 @@
+/*
+Template Name: YEGNA - Responsive 5 Admin Dashboard
+Author: MERKL>DEV
+File: wizard Js File
+*/
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+window.bootstrap = bootstrap
+
+import Wizard from './wizard'
+
+new Wizard('#horizontalwizard');
+
+new Wizard('#verticalwizard');
